ccf769efc24a260e8ced364b2bd193ee29e01fa0
carrowkeel <6824521+carrowkeel@users.noreply.github.com>
2022-12-13 16:05:26 +0200

    move general functions to init.js

--- init.js
+++ init.js
@@ -1,5 +1,55 @@
 'use strict';
 
+const loadScript = (script, type="script") => new Promise(resolve => {
+	if (document.head.querySelectorAll(`script[src="${script}"]`).length > 0)
+		return resolve();
+	const elem = document.createElement(type);
+	type === 'link' ? elem.href = script : elem.src = script;
+	elem.addEventListener('load', () => resolve());
+	document.head.appendChild(elem);
+});
+
+const parseJSON = (text, default_value={}) => {
+	try {
+		if (text === null)
+			throw 'JSON string missing';
+		return JSON.parse(text);
+	} catch (e) {
+		return default_value;
+	}
+};
+
+const getCredentials = (type) => { // Add same or combined for setting credentials
+	if (!type)
+		return ['user_id', 'token', 'cdn'].reduce((credentials, key) => Object.assign(credentials, {[key]: getCredentials(key)}), {});
+	switch(type) {
+		case 'user_id':
+			return localStorage.getItem('mdx_user') || false;
+		case 'token':
+			return localStorage.getItem('mdx_token') || false;
+		case 'cdn':
+			return parseJSON(localStorage.getItem('mdx_signature'), false);
+	}
+};
+
+const signedURL = (url, query = {}) => {
+	const signature = getCredentials('cdn');
+	if (!signature)
+		return url;
+	const qs = new URLSearchParams(Object.assign({}, query, signature));
+	return `${url}?${qs.toString()}`;
+};
+
+const addModule = (elem, name, options={}, replace_element=false) => new Promise((resolve, reject) => {
+	const module = replace_element ? elem : elem.appendChild(document.createElement('div'));
+	module.dataset.module = name;
+	Object.keys(options).forEach(k => typeof options[k] !== 'object' ? module.dataset[k] = options[k] : 0);
+	module.dispatchEvent(new CustomEvent('render', {detail: {options}}));
+	module.addEventListener('done', e => {
+		resolve({module, data: e.detail});
+	});
+});
+
 const initServiceWorker = (uri) => new Promise((resolve, reject) => {
 	if ('serviceWorker' in navigator) {
 		return navigator.serviceWorker.register(uri, {scope: '/'}).then(reg => {
--- modelrxiv.js
+++ modelrxiv.js
@@ -2,36 +2,7 @@
 
 const numericize = v => Array.isArray(v) ? v.map(numericize) : typeof v === 'object' ? Object.keys(v).reduce((a,_v)=>Object.assign(a, {[_v]: numericize(v[_v])}), {}) : (v !== '' && v !== null && !isNaN(v) ? +(v) : v);
 
-const loadScript = (script, type="script") => new Promise(resolve => {
-	if (document.head.querySelectorAll(`script[src="${script}"]`).length > 0)
-		return resolve();
-	const elem = document.createElement(type);
-	type === 'link' ? elem.href = script : elem.src = script;
-	elem.addEventListener('load', () => resolve());
-	document.head.appendChild(elem);
-});
-
-const parseJSON = (text, default_value={}) => {
-	try {
-		if (text === null)
-			throw 'JSON string missing';
-		return JSON.parse(text);
-	} catch (e) {
-		return default_value;
-	}
-};
-
 const queryFromPath = (defaults = {}) => numericize(window.location.pathname.substring(1).split('/').reduce((a,v,i,arr)=>i%2===0&&arr[i+1]!==undefined?Object.assign(a, {[v]: arr[i+1]}):a, defaults));
-
-const addModule = (elem, name, options={}, replace_element=false) => new Promise((resolve, reject) => {
-	const module = replace_element ? elem : elem.appendChild(document.createElement('div'));
-	module.dataset.module = name;
-	Object.keys(options).forEach(k => typeof options[k] !== 'object' ? module.dataset[k] = options[k] : 0);
-	module.dispatchEvent(new CustomEvent('render', {detail: {options}}));
-	module.addEventListener('done', e => {
-		resolve({module, data: e.detail});
-	});
-});
 
 const staticDB = options => ({
 	data: {},
@@ -62,14 +33,6 @@ const staticDB = options => ({
 		});
 	}
 });
-
-const signedURL = (url, query = {}) => {
-	const signature = getCredentials('cdn');
-	if (!signature)
-		return url;
-	const qs = new URLSearchParams(Object.assign({}, query, signature));
-	return `${url}?${qs.toString()}`;
-};
 
 const addHooks = (elem, hooks) => {
 	for (const type of Object.keys(hooks.reduce((a,v)=>Object.assign(a, {[v[1]]: 1}), {}))) {
@@ -336,20 +299,6 @@ const auth = (env, login={}) => {
 		console.log(e);
 	});
 };
-
-const getCredentials = (type) => { // Add same or combined for setting credentials
-	if (!type)
-		return ['user_id', 'token', 'cdn'].reduce((credentials, key) => Object.assign(credentials, {[key]: getCredentials(key)}), {});
-	switch(type) {
-		case 'user_id':
-			return localStorage.getItem('mdx_user') || false;
-		case 'token':
-			return localStorage.getItem('mdx_token') || false;
-		case 'cdn':
-			return parseJSON(localStorage.getItem('mdx_signature'), false);
-	}
-};
-
 
 export const modelrxiv = async () => {
 	const env = {db: staticDB({uri: 'https://modelrxiv.org'}), processes: {}, timeouts: {}};
